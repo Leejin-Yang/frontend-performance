a4404b22fda8097ae087616a3fe67067a21bb858
Leejin-Yang <ejin1996@gmail.com>
2023-08-05 21:21:01 +0900

    chore: 기본 설정

---
+++ webpack.common.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const BundleAnalyzerPlugin =
+  require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true,
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', 'json'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx|js)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpeg|jpg)$/,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [new BundleAnalyzerPlugin()],
+};
