8ebd02d5ab1353eea3fd2a25981d84811c04944b
Leejin-Yang <ejin1996@gmail.com>
2023-08-09 04:08:51 +0900

    반응형 이미지

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <img
+        srcSet='/assets/sam-300w.png 300w, /assets/sam-600w.png 600w, /assets/sam-900w.png 900w'
+        sizes='(max-width: 300px) 300px, (max-width: 600px) 600px, (max-width: 900px) 900px'
+        src='/assets/sam-900w.png'
+        alt='펀잇 삼김이'
+      />
+    </div>
+  );
 };
 
 export default App;
